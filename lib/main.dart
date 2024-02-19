6bacf458b4cfda13676f48fc181a04e7002a708a
Azhar <103309031+SpiritGitHub@users.noreply.github.com>
2024-02-19 15:05:07 +0000

    liaison

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:eat_g/themes/light_theme.dart';
 import 'package:eat_g/routes/routes.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   // await dotenv.load(fileName: ".env");
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,10 @@ import 'package:eat_g/utils/build_context_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,6 +15,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final TextEditingController _ingredientsController = TextEditingController();
+
+  static const String apiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
+
+  Future<dynamic> getRecipes(String ingredients) async {
+    final response = await http.get(Uri.parse('$apiUrl$ingredients'));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body); // Handle response
+    } else {
+      throw Exception('Erreur lors de la récupération des recettes');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -101,8 +119,14 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Center(
               child: ElevatedButton(
-                onPressed: () {
-                  context.push("/result");
+                onPressed: () async {
+                  final ingredients = _ingredientsController.text.trim();
+                  try {
+                    final results = await getRecipes(ingredients);
+                    context.pushNamed('/result', extra: results);
+                  } on Exception catch (e) {
+                    // Handle API errors
+                  }
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: context.primary,
